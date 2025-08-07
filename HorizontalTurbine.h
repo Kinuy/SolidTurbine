fa83f64bf69e851ab54c7c111a95db8cad218aeb
Tobias Weber <toby-weber@gmx.de>
2025-08-07 18:28:00 +0200

    fat: Add concrete implementation of horizontal turbine

---
+++ HorizontalTurbine.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "ITurbine.h"
+
+// Concrete horizontal turbine implementation
+class HorizontalTurbine : public ITurbine
+{
+private:
+
+	int numberOfBlades; // Number of blades in the turbine
+
+public:
+
+	HorizontalTurbine(const int blades);
+
+	// Override the getNumberOfBlades method from ITurbine interface
+	int getNumberOfBlades() const override;
+};
+
