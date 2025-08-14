1ba520fdcbe9e0210c17877f06e2068f83ada8f6
Tobias Weber <toby-weber@gmx.de>
2025-08-14 17:11:11 +0200

    fix: Extend to 3D

--- AirfoilCoordinate.h
+++ AirfoilCoordinate.h
@@ -5,6 +5,12 @@
 struct AirfoilCoordinate {
     double x;
     double y;
+	double z; // Optional z-coordinate for 3D representation
 
-    AirfoilCoordinate(double xVal, double yVal) : x(xVal), y(yVal) {}
+    AirfoilCoordinate(double xVal, double yVal, double zVal=0) : x(xVal), y(yVal), z(zVal) {}
+
+    bool is2D() const { return z == 0.0; }
+    
+    bool is3D() const { return z != 0.0; }
+
 };
