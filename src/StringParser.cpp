65f2bdd7b68d2fcf0ffdfba1cb602b74c89127d6
Tobias Weber <toby-weber@gmx.de>
2025-08-18 18:42:31 +0200

    refactor: Move implementation to cpp file

--- src/StringParser.cpp
+++ src/StringParser.cpp
@@ -1,1 +1,10 @@
 #include "StringParser.h"
+
+std::any StringParser::parse(const std::string& value) const {
+    return value;
+}
+
+std::string StringParser::getTypeName() const { 
+    return "string"; 
+}
+
