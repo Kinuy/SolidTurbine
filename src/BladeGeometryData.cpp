f99ab487f234637849fbb49ecf2f9e7850bb19d6
Tobias Weber <toby-weber@gmx.de>
2025-08-22 13:21:05 +0200

    refactor: Uncomment commented methods

--- src/BladeGeometryData.cpp
+++ src/BladeGeometryData.cpp
@@ -16,33 +16,33 @@ std::string BladeGeometryData::getTypeName() const { return "BladeGeometry"; }
 
 size_t BladeGeometryData::getRowCount() const { return rows.size(); }
 
-//BladeGeometrySection& BladeGeometryData::getRowByRadius(double radius, double tolerance) const {
-//    auto it = std::find_if(rows.begin(), rows.end(),
-//        [radius, tolerance](const BladeGeometrySection& row) {
-//            return std::abs(row.bladeRadius - radius) <= tolerance;
-//        });
-//
-//    if (it == rows.end()) {
-//        throw std::runtime_error("No blade geometry found for radius: " + std::to_string(radius));
-//    }
-//    return *it;
-//}
-//
-//BladeGeometrySection& BladeGeometryData::getRowByRelativeThickness(double relativeThickness, double tolerance) const {
-//    auto it = std::find_if(rows.begin(), rows.end(),
-//        [relativeThickness, tolerance](const BladeGeometrySection& row) {
-//            return std::abs(row.relativeThickness - relativeThickness) <= tolerance;
-//        });
-//
-//    if (it == rows.end()) {
-//        throw std::runtime_error("No blade geometry found for relativeThickness: " + std::to_string(relativeThickness));
-//    }
-//    return *it;
-//}
-//
-//BladeGeometrySection& BladeGeometryData::getRowByIndex(int index) const{
-//        return rows.at(index);
-//}
+BladeGeometrySection& BladeGeometryData::getRowByRadius(double radius, double tolerance){
+    auto it = std::find_if(rows.begin(), rows.end(),
+        [radius, tolerance](const BladeGeometrySection& row) {
+            return std::abs(row.bladeRadius - radius) <= tolerance;
+        });
+
+    if (it == rows.end()) {
+        throw std::runtime_error("No blade geometry found for radius: " + std::to_string(radius));
+    }
+    return *it;
+}
+
+BladeGeometrySection& BladeGeometryData::getRowByRelativeThickness(double relativeThickness, double tolerance){
+    auto it = std::find_if(rows.begin(), rows.end(),
+        [relativeThickness, tolerance](const BladeGeometrySection& row) {
+            return std::abs(row.relativeThickness - relativeThickness) <= tolerance;
+        });
+
+    if (it == rows.end()) {
+        throw std::runtime_error("No blade geometry found for relativeThickness: " + std::to_string(relativeThickness));
+    }
+    return *it;
+}
+
+BladeGeometrySection& BladeGeometryData::getRowByIndex(int index){
+        return rows.at(index);
+}
 
 std::vector<double> BladeGeometryData::getRadiusValues() const {
     std::vector<double> radii;
