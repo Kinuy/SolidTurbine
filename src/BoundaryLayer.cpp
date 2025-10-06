75e5590bc7f62350915c2c28ab9d25a2a2228211
Tobias Weber <toby-weber@gmx.de>
2025-10-06 13:32:15 +0200

    feat: Add boundaryLayer data manager

---
+++ src/BoundaryLayer.cpp
@@ -0,0 +1,26 @@
+#include "BoundaryLayer.h"
+
+bool BoundaryLayer::recalculate(const Eigen::Vector3d& freestreamVelocity, const Eigen::MatrixXd& surfaceVelocities)
+{
+    return true;
+}
+
+double BoundaryLayer::thickness(const std::shared_ptr<Surface>& surface, int panel) const
+{
+    return 0.0;
+}
+
+Eigen::Vector3d BoundaryLayer::velocity(const std::shared_ptr<Surface>& surface, int panel, double y) const
+{
+    return Eigen::Vector3d(0, 0, 0);
+}
+
+double BoundaryLayer::transpirationVelocity(const std::shared_ptr<Surface>& surface, int panel) const
+{
+    return 0.0;
+}
+
+Eigen::Vector3d BoundaryLayer::friction(const std::shared_ptr<Surface>& surface, int panel) const
+{
+    return Eigen::Vector3d(0, 0, 0);
+}
